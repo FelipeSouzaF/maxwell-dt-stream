36b6b0d806c89fa7eb655e7c7af7ba119d48fee2
Felipe Farias <felipesouzaf11@gmail.com>
2023-06-14 23:34:39 -0300

    feat: add dynamodb insert data

--- consumer/app.js
+++ consumer/app.js
@@ -1,11 +1,34 @@
 import { Consumer } from 'sqs-consumer';
+import AWS from 'aws-sdk';
+import { v4 as uuidv4 } from 'uuid';
 
 const app = Consumer.create({
   queueUrl: 'http://localhost:9324/queue/default',
   handleMessage: async (message) => {
     // do some work with `message`
-    console.log('process2')
+    console.log('event')
     console.log(message)
+
+    const dynamodb = new AWS.DynamoDB({
+      endpoint: "http://localhost:8000",
+    });
+    const docClient = new AWS.DynamoDB.DocumentClient({service: dynamodb});
+
+    const params = {
+      TableName: 'pedidos',
+      Item: {
+        'id': uuidv4(),
+        'json_value': JSON.stringify(message),
+      },
+    };
+    
+    docClient.put(params, function(err, data) {
+      if (err) {
+        console.log("Error", err);
+      } else {
+        console.log("Success", data);
+      }
+    });
   }
 });
 
